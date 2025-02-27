bf1a9344a8753a0dfc11ca0e4045f29a559c668c
Richard Fisher <richard.fisher@what3words.com>
2025-02-27 16:18:15 +0000

    Add basic tests for materialize call extraction

---
+++ tests/test_extraction_of_materialize_calls.py
@@ -0,0 +1,47 @@
+from src.dbt_metric_utils.helpers import _extract_materialize_calls
+
+
+def test_dimension_in_call():
+    input_sql = """
+    
+    select * from {{ dbt_metric_utils_materialize(
+      metrics=['metric']
+      , where="{{Dimension('dim')}}=1"
+    ) }}
+    
+    """
+
+    expected_sql = [
+        """dbt_metric_utils_materialize(
+      metrics=['metric']
+      , where="{{Dimension('dim')}}=1"
+    )"""
+    ]
+
+    actual_sql = _extract_materialize_calls(input_sql)
+    assert expected_sql == actual_sql
+
+
+def test_multiple_calls():
+    input_sql = """
+
+    select * from {{ dbt_metric_utils_materialize(
+      metrics=['metric_1']
+    ) }}
+
+    select * from {{ dbt_metric_utils_materialize(
+      metrics=['metric_2']
+    ) }}
+    """
+
+    expected_sql = [
+        """dbt_metric_utils_materialize(
+      metrics=['metric_1']
+    )""",
+        """dbt_metric_utils_materialize(
+      metrics=['metric_2']
+    )"""
+    ]
+
+    actual_sql = _extract_materialize_calls(input_sql)
+    assert expected_sql == actual_sql
