8ffa556b8788946cc03d34a4b989c1c25555bb7a
Manjula Nagesh <manjula.nagesh@what3words.com>
2025-03-27 17:14:23 +0000

    Added proto events

--- src/dbt_metric_utils/custom_events_pb2.py
+++ src/dbt_metric_utils/custom_events_pb2.py
@@ -16,17 +16,29 @@ _sym_db = _symbol_database.Default()
 from google.protobuf import timestamp_pb2 as google_dot_protobuf_dot_timestamp__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x13\x63ustom_events.proto\x12\x0bproto_types\x1a\x1fgoogle/protobuf/timestamp.proto\"K\n\x19MetricUtilsInterceptStart\x12.\n\nstarted_at\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\"j\n\"MetricUtilsInterceptInvokeOriginal\x12.\n\nstarted_at\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x14\n\x0c\x65lapsed_time\x18\x02 \x01(\x02\"g\n\x1dMetricUtilsInterceptCompleted\x12\x30\n\x0c\x63ompleted_at\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x14\n\x0c\x65lapsed_time\x18\x02 \x01(\x02\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x13\x63ustom_events.proto\x12\x0bproto_types\x1a\x1fgoogle/protobuf/timestamp.proto\x1a\x1cgoogle/protobuf/struct.proto\"\x99\x02\n\rCoreEventInfo\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0c\n\x04\x63ode\x18\x02 \x01(\t\x12\x0b\n\x03msg\x18\x03 \x01(\t\x12\r\n\x05level\x18\x04 \x01(\t\x12\x15\n\rinvocation_id\x18\x05 \x01(\t\x12\x0b\n\x03pid\x18\x06 \x01(\x05\x12\x0e\n\x06thread\x18\x07 \x01(\t\x12&\n\x02ts\x18\x08 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x34\n\x05\x65xtra\x18\t \x03(\x0b\x32%.proto_types.CoreEventInfo.ExtraEntry\x12\x10\n\x08\x63\x61tegory\x18\n \x01(\t\x1a,\n\nExtraEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\"K\n\x19MetricUtilsInterceptStart\x12.\n\nstarted_at\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\"~\n\x1cMetricUtilsInterceptStartMsg\x12(\n\x04info\x18\x01 \x01(\x0b\x32\x1a.proto_types.CoreEventInfo\x12\x34\n\x04\x64\x61ta\x18\x02 \x01(\x0b\x32&.proto_types.MetricUtilsInterceptStart\"j\n\"MetricUtilsInterceptInvokeOriginal\x12.\n\nstarted_at\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x14\n\x0c\x65lapsed_time\x18\x02 \x01(\x02\"\x90\x01\n%MetricUtilsInterceptInvokeOriginalMsg\x12(\n\x04info\x18\x01 \x01(\x0b\x32\x1a.proto_types.CoreEventInfo\x12=\n\x04\x64\x61ta\x18\x02 \x01(\x0b\x32/.proto_types.MetricUtilsInterceptInvokeOriginal\"g\n\x1dMetricUtilsInterceptCompleted\x12\x30\n\x0c\x63ompleted_at\x18\x01 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\x14\n\x0c\x65lapsed_time\x18\x02 \x01(\x02\"\x86\x01\n MetricUtilsInterceptCompletedMsg\x12(\n\x04info\x18\x01 \x01(\x0b\x32\x1a.proto_types.CoreEventInfo\x12\x38\n\x04\x64\x61ta\x18\x02 \x01(\x0b\x32*.proto_types.MetricUtilsInterceptCompletedb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'custom_events_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
   DESCRIPTOR._loaded_options = None
-  _globals['_METRICUTILSINTERCEPTSTART']._serialized_start=69
-  _globals['_METRICUTILSINTERCEPTSTART']._serialized_end=144
-  _globals['_METRICUTILSINTERCEPTINVOKEORIGINAL']._serialized_start=146
-  _globals['_METRICUTILSINTERCEPTINVOKEORIGINAL']._serialized_end=252
-  _globals['_METRICUTILSINTERCEPTCOMPLETED']._serialized_start=254
-  _globals['_METRICUTILSINTERCEPTCOMPLETED']._serialized_end=357
+  _globals['_COREEVENTINFO_EXTRAENTRY']._loaded_options = None
+  _globals['_COREEVENTINFO_EXTRAENTRY']._serialized_options = b'8\001'
+  _globals['_COREEVENTINFO']._serialized_start=100
+  _globals['_COREEVENTINFO']._serialized_end=381
+  _globals['_COREEVENTINFO_EXTRAENTRY']._serialized_start=337
+  _globals['_COREEVENTINFO_EXTRAENTRY']._serialized_end=381
+  _globals['_METRICUTILSINTERCEPTSTART']._serialized_start=383
+  _globals['_METRICUTILSINTERCEPTSTART']._serialized_end=458
+  _globals['_METRICUTILSINTERCEPTSTARTMSG']._serialized_start=460
+  _globals['_METRICUTILSINTERCEPTSTARTMSG']._serialized_end=586
+  _globals['_METRICUTILSINTERCEPTINVOKEORIGINAL']._serialized_start=588
+  _globals['_METRICUTILSINTERCEPTINVOKEORIGINAL']._serialized_end=694
+  _globals['_METRICUTILSINTERCEPTINVOKEORIGINALMSG']._serialized_start=697
+  _globals['_METRICUTILSINTERCEPTINVOKEORIGINALMSG']._serialized_end=841
+  _globals['_METRICUTILSINTERCEPTCOMPLETED']._serialized_start=843
+  _globals['_METRICUTILSINTERCEPTCOMPLETED']._serialized_end=946
+  _globals['_METRICUTILSINTERCEPTCOMPLETEDMSG']._serialized_start=949
+  _globals['_METRICUTILSINTERCEPTCOMPLETEDMSG']._serialized_end=1083
 # @@protoc_insertion_point(module_scope)
